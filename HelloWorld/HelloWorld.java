ac8242f5b149cffc8102b83bcac8a675f0c9ba99
陆方七安 <104905757+LUFANGQIAN@users.noreply.github.com>
2024-10-29 20:53:36 +0800

    Add files via upload

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+// Hello.java
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.print("I am Hello Class, which is a main class");
+    }
+}
+
+class E {
+    public static void main(String[] args) {
+        System.out.print("I am E Class, which is a main class");
+    }
+}
+
